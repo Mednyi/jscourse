dde9d4ab77dc758757c4601e27c6e007ad08c7dc
mytestkube <mednyi.vsadnik@gmail.com>
2020-07-08 10:32:37 +0300

    'initial'

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict';
+// Comment
+/*
+Comments
+ */
+
+// Переменные
+// Объявление переменной
+let localVariable = 5;
+// Инициализация
+localVariable = 10;
+
+// Deprecated.
+var globalVariable = 100;
+
+// Constant
+const constVariable = "This is constant var";
+const CONSTANT_VARIABLE = 10;
+
+
+// Data types
+
+// Number
+let number = 10; // maximum = 2^53        minimum = -2^53
+number = 2.5;
+number = 1/0; // (Infinity)
+let notANumber = 'try it' / 10; // (NaN)
+const bigInteger = 72834782738472834782673678534897589347589739847589n; // big integer. infinity. literal n at the end.
+
+// Strings
+let str = 'This is string';
+str = "This is string";
+const strConst = 'This is constant string ';
+let date = new Date().toLocaleDateString();
+let templateString = `Today is ${date}
+    A good day
+`;
+
+// Boolean
+let trueStatement = true;
+let falseStatement = false;
