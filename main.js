511131a25d870319f2a64bb1f3450341a0bc9385
mytestkube <mednyi.vsadnik@gmail.com>
2020-07-08 11:24:08 +0300

    'lesson1'

--- main.js
+++ main.js
@@ -37,5 +37,37 @@ let templateString = `Today is ${date}
 `;
 
 // Boolean
+
 let trueStatement = true;
 let falseStatement = false;
+
+// null - type
+
+let nullVar = null;
+
+// undefined
+
+let undefVar; // initial value is undefined
+
+// Object
+
+let tank = {
+    ammo: 100,
+    armor: 100,
+    fire () {
+        this.ammo--;
+    }
+};
+tank.ammo = 50;
+tank.armor = 100;
+
+// Operator typeof - check variable type
+
+typeof tank; // object
+
+// Array is object with number keys
+
+let arr = [];
+arr = [12, 'asdfasdf', {a:10}];
+console.log(arr[1]); // console output : asdfasdf
+
