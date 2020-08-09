6a16f9d01d1415e5786c5fb3697a08d93a0af65e
yashtole <imova.maria@gmail.com>
2020-08-10 01:23:38 +0300

    Методы класса HTTPRequest

--- homework/5_6_homework/5_homework.js
+++ homework/5_6_homework/5_homework.js
@@ -38,38 +38,67 @@ path будет Dante
 
 data = {
     "drinks.ru/sale/tea": {
-            type: 'green',
-            area: 'Ceylon',
-            amt: 2
-        },
+        type: 'green',
+        area: 'Ceylon',
+        amt: 2
+    },
     "drinks.ru/sale/coffe": {
-            type: 'grain',
-            area: 'Kenia',
-            amt: 5
-        }
+        type: 'grain',
+        area: 'Kenia',
+        amt: 5
+    }
 };
 
+const createItem = (body = this.body, type = undefined, area = undefined, amt = undefined) => ({ [body.value]: { type, area, amt } });
+
+const getData = path => new Promise((resolve, reject) => {
+    const result = data[path];
+    setTimeout(resolve, 5000, result);
+});
 
 
 class HTTPRequest {
-    constructor (basePath = '') {
-        basePath = this.basePath;
+    constructor(basePath = '') {
+        this.basePath = basePath
     }
-    async get(path) {
-        const getData = path => new Promise((resolve, reject) => {
-            const result = data[path];
-            setTimeout(resolve, 5000, result);
-        });
-        const response = await getData(basePath+path);
-        return response;
-    };
+    async get(path = '') {
+        const response = await getData(this.basePath + path)
+        console.log(response)
+        return response
+    }
     async post(path, body) {
-        const data = get(path);
-        const body = data[basePath+path];
-        if 
+        const data = get(path)
+        const body = data[this.basePath + path]
+        if (data.body != undefined) {
+            console.log('такой напиток уже добавлен')
         }
-        return {folders, files};
+        else {
+            const newItem = createItem(this.body)
+            data.push(newItem)
+        };
+        return data
     };
-    async put(path, body) {};
-    async deleteData(path) {};
+    async put(path, body) { 
+        const data = get(path)
+        const body = data[this.basePath + path]
+        if (data.body = undefined) {
+            console.log('такой напиток еще не добавлен')
+        }
+        else {
+            
+        }
+        return data
+
+    };
+    async deleteData(path) { 
+        const data = get(path)
+        const body = data[this.basePath + path]
+        if (data.body = undefined) {
+            console.log('такой напиток еще не добавлен')
+        }
+        else {
+            
+        }
+        return data
+    };
 };
