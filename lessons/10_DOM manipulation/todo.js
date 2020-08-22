47e9da53fbedf91d70f20cc652e8dca4e688c669
mytestkube <mednyi.vsadnik@gmail.com>
2020-08-22 11:17:15 +0300

    'Shop-homework-page'

--- lessons/10_DOM manipulation/todo.js
+++ lessons/10_DOM manipulation/todo.js
@@ -43,9 +43,50 @@ class todoView {
     }
 }
 
-const todos = document.getElementsByTagName('main')[0];
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', function(e) {
-    const todo = new todoView();
-    todos.appendChild(todo.render());
-});
+class todosView {
+    constructor(data = {todos:[], filter:''}) {
+        this.data = data;
+        this.addTodo = this.addTodo.bind(this);
+        this.filter = this.filter.bind(this);
+    }
+    template () {
+        return `
+                <header class="todo" id="toolbar">
+                    <input type="text" placeholder="filter">
+                    <div id="add" class="button">Добавить</div>
+                </header>
+        `
+    }
+    addTodo () {
+        this.data.todos.push({ title: '', done: false });
+        this.render();
+    }
+    filter () {
+        const filterInput = this.$el.firstElementChild.getElementsByTagName('input')[0];
+        this.data.filter = filterInput.value;
+        this.render();
+    }
+    render () {
+        // Check if element already exists, if not - create one
+        if (!this.$el) {
+            this.$el = document.createElement('main');
+        }
+        // Remove div wrapper element
+        this.$el.innerHTML = this.template();
+        // Add event listeners to elements
+        const addButton = this.$el.firstElementChild.getElementsByTagName('div')[0];
+        const filterInput = this.$el.firstElementChild.getElementsByTagName('input')[0];
+        filterInput.value = this.data.filter;
+        addButton.addEventListener('click', this.addTodo);
+        filterInput.addEventListener('input', this.filter);
+        // Render child elements
+        const filtered = this.data.filter ? this.data.todos.filter(todo => todo.title.includes(this.data.filter)) : this.data.todos;
+        filtered.forEach(todo => {
+           const view = new todoView(todo);
+           this.$el.appendChild(view.render());
+        });
+        return this.$el
+    }
+}
+
+document.getElementsByTagName('body')[0].appendChild(new todosView().render());
