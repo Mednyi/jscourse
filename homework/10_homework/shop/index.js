f955e11644ccf6a86c6bc8efc3a824cd59e7ec9f
mytestkube <mednyi.vsadnik@gmail.com>
2020-08-31 10:31:30 +0300

    'components'

--- homework/10_homework/shop/index.js
+++ homework/10_homework/shop/index.js
@@ -2,108 +2,149 @@
 /*
 Реализовать добавление, фильтрацию, удаление, редактирование товаров. По клику на buy - увеличивать счётчик товаров в корзине.
 */
+let products = [
+    {
+        productName: 'Suspendisse potenti',
+        productDescription: 'Suspendisse potenti. Nullam vehicula elit ac purus ullamcorper finibus.'
+    },
+    {
+        productName: 'Integer placerat',
+        productDescription: 'Integer placerat sollicitudin odio, sed molestie tortor auctor nec.'
+    }
+];
+
+class Component {
+    constructor(data = {}) {
+        this.data = data;
+        Object.keys(this.methods).forEach(methodName => this.methods[methodName] = this.methods[methodName].bind(this));
+    }
+    template () {return ''}
+    methods = {}
+    onRender () {}
+    render () {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.template();
+        if (this.$el) {
+            this.$el.innerHTML = wrapper.firstElementChild.innerHTML
+        } else {
+            this.$el = wrapper.firstElementChild
+        }
+        wrapper.remove();
+        this.onRender();
+    }
+
+}
 
 class cardView {
     constructor(data = { productName: '', productDescription: '' }) {
         this.data = data;
-        this.rename = this.rename.bind(this);
-        this.changeDescription = this.changeDescription.bind(this);
-        this.remove = this.remove.bind(this);
+        Object.keys(this.methods).forEach(methodName => this.methods[methodName] = this.methods[methodName].bind(this));
     }
     template() {
         return `
-    <main>
-        <div class="card">
-            <h4><input type="text" placeholder="Product name" value="${this.data.productName}"><span>X</span></h4>
-            <div><textarea rows="2" placeholder="Product description" value="${this.data.productDescription}"></textarea></div>
-            <div class="button">Buy</div>
-        </div>
-    </main>
+            <div class="card">
+                <h4><input type="text" placeholder="Product name" value="${this.data.productName}"><span>X</span></h4>
+                <div><textarea rows="2" placeholder="Product description" value="${this.data.productDescription}"></textarea></div>
+                <div class="button">Buy</div>
+            </div>
         `
     }
-    remove() {
-        this.$el.remove();
-    }
-    rename() {
-        this.data.productName = this.$el.firstElementChild.getElementsByTagName('input')[0].value;
-        this.render();
-    }
-    changeDescription() {
-        this.data.productDescription = this.$el.firstElementChild.getElementsByTagName('textarea')[0].value;
-        this.render();
+    methods = {
+        remove () {
+            this.$el.remove();
+            const index = products.findIndex(product => product.productName === this.data.productName);
+            products.splice(index, 1);
+        },
+        rename () {
+            this.data.productName = this.$el.firstElementChild.getElementsByTagName('input')[0].value;
+            this.render();
+        },
+        changeDescription () {
+            this.data.productDescription = this.$el.firstElementChild.getElementsByTagName('textarea')[0].value;
+            this.render();
+        },
     }
     render() {
-        if (!this.$el) {
-            this.$el = document.createElement('main');
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.template();
+        if (this.$el) {
+            this.$el.innerHTML = wrapper.firstElementChild.innerHTML
+        } else {
+            this.$el = wrapper.firstElementChild
         }
-        this.$el.innerHTML = this.template();
-        this.$el = this.$el.firstElementChild;
+        wrapper.remove();
         const productNameInput = this.$el.getElementsByTagName('input')[0];
         const productDescriptionInput = this.$el.getElementsByTagName('textarea')[0];
-        const buyButton = this.$el.getElementsByTagName('div')[1]
-        productNameInput.addEventListener('blur', this.rename);
-        productDescriptionInput.addEventListener('blur', this.changeDescription);
-        buyButton.addEventListener('click', this.buy);
+        const deleteButton = this.$el.firstElementChild.getElementsByTagName('span')[0];
+        deleteButton.addEventListener('click', this.methods.remove);
+        productNameInput.addEventListener('blur', this.methods.rename);
+        productDescriptionInput.addEventListener('blur', this.methods.changeDescription);
         return this.$el
     }
 }
 
-class headerView {
-    constructor(data = { products: [], filter: '', itemsInCart: 0 }) {
+class cardsView {
+    constructor(data = {filter: '', products}) {
         this.data = data;
+        this.data.count = 0;
         this.addProduct = this.addProduct.bind(this);
         this.filter = this.filter.bind(this);
         this.buy = this.buy.bind(this);
     }
-    template() {
-        return `
-    <header>
-        <div class="logo">My SuperSHop</div>
-        <div class="controls">
-            <input type="text" placeholder="Filter">
-            <div>
-               <span>Items in cart:${this.data.itemsInCart}</span><span> 0</span>
-            </div>
-            <div class="button">Add new product</div>
-        </div>
-    </header>
-        `
+    template () {
+        return ` 
+                  <div class="shop_component">  
+                    <header>
+                         <div class="logo">My SuperSHop</div>
+                         <div class="controls">
+                                <input type="text" placeholder="Filter" value="${this.data.filter}">
+                                <div>
+                                    <span>Items in cart:</span><span> ${this.data.count}</span>
+                                </div>
+                                <div class="button">Add new product</div>
+                        </div>
+                    </header>
+                    <main>
+                    </main>
+                  </div>  
+                `
     }
-    addProduct() {
+    buy () {
+        this.data.count++;
+        this.render();
+    }
+    addProduct () {
         this.data.products.push({ productName: '', productDescription: '' });
         this.render();
     }
     filter() {
-        const filterInput = this.$el.children[1].getElementsByTagName('input')[0];
+        const filterInput = this.$el.firstElementChild.children[1].getElementsByTagName('input')[0];
         this.data.filter = filterInput.value;
         this.render();
     }
-    buy() {
-        this.data.itemsInCart = this.$el.children[1].getElementsByTagName('span')[1].value + 1;
-        this.render();
-    }
     render() {
-        if (!this.$el) {
-            this.$el = document.createElement('header');
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.template();
+        if (this.$el) {
+            this.$el.innerHTML = wrapper.firstElementChild.innerHTML
+        } else {
+            this.$el = wrapper.firstElementChild
         }
-        // Remove div wrapper element
-        this.$el.innerHTML = this.template();
-        // Add event listeners to elements
-        const addButton = this.$el.children[1].getElementsByTagName('div')[0];
-        const filterInput = this.$el.children[1].getElementsByTagName('input')[0];
-        const itemsInCart = this.$el.children[1].getElementsByTagName('span')[1]
-        filterInput.value = this.data.filter;
+        wrapper.remove();
+        const addButton = this.$el.firstElementChild.getElementsByClassName('button')[0];
+        const filterInput = this.$el.firstElementChild.children[1].getElementsByTagName('input')[0];
         addButton.addEventListener('click', this.addProduct);
-        itemsInCart.addEventListener('click', this.buy);
-        // Render child elements
-        const filtered = this.data.filter ? this.data.products.filter(product => product.title.includes(this.data.filter)) : this.data.products ;
+        filterInput.addEventListener('blur', this.filter);
+        const filtered = this.data.filter ? this.data.products.filter(product => product.productName.includes(this.data.filter)) : this.data.products ;
         filtered.forEach(product => {
             const view = new cardView(product);
-            this.$el.appendChild(view.render());
+            const view_el = view.render();
+            this.$el.getElementsByTagName('main')[0].appendChild(view_el);
+            const buyButton = view_el.getElementsByTagName('div')[1];
+            buyButton.addEventListener('click', this.buy);
         });
         return this.$el
     }
 }
 
-document.getElementsByTagName('div')[0].appendChild(new headerView().render());
-document.getElementsByTagName('div')[0].appendChild(new cardView().render());   
+document.getElementsByTagName('body')[0].appendChild(new cardsView().render());
