f9bdb9378e622755f97886af0a7ba223dac3c8c6
mytestkube <mednyi.vsadnik@gmail.com>
2020-09-27 20:57:24 +0300

    'last'

--- homework/10_homework/chess/src/modules/Header.js
+++ homework/10_homework/chess/src/modules/Header.js
@@ -8,11 +8,25 @@ export default class Header extends Component {
         return `
         <header>
             <div>&#9819; Amazing Checkmates</div>
-            <div>
+            <div id="burger" tabindex="1">
                 <div class="burger"></div>
                 <div class="burger2"></div>
                 <div class="burger3"></div>
             </div>
+            <aside>
+                <div>
+                    Menu ITem1
+                </div>
+                <div>
+                    Menu ITem2
+                </div>
+                <div>
+                    Menu ITem3
+                </div>
+                <div>
+                    Menu ITem4
+                </div>
+            </aside>
         </header>
         `
     }
